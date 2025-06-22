1712f7c5394fdf99a83abba866f0deb35e634d79
Pedro Henriques <pedro@pedrojhenriques.com>
2025-06-22 17:13:27 +0000

    Improve the Tester project to use strong types in the document used for testing.

--- setup/local/Tester/Program.cs
+++ setup/local/Tester/Program.cs
@@ -4,6 +4,8 @@ using Toolkit;
 using Toolkit.Types;
 using FFUtils = Toolkit.Utils.FeatureFlags;
 using LoggerUtils = Toolkit.Utils.Logger;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -37,12 +39,14 @@ if (app.Environment.IsDevelopment())
   app.UseSwaggerUI();
 }
 
-dynamic document = new ExpandoObject();
-document.prop1 = "value 1";
-document.prop2 = "value 2";
-document.prop3 = true;
-document.prop4 = 4.86;
-document.prop5 = 10;
+MyValue document = new MyValue
+{
+  Prop1 = "value 1",
+  Prop2 = "value 2",
+  Prop3 = true,
+  Prop4 = 4.86,
+  Prop5 = 10,
+};
 
 IFeatureFlags featureFlags = app.Services.GetService<IFeatureFlags>();
 
@@ -61,3 +65,33 @@ logger.Log(LogLevel.Information, null, "Tester: setup complete.");
 logger.Log(LogLevel.Critical, new Exception("Tester: test exception for log"), "Tester: exception logging.");
 
 app.Run();
+
+public class MyKey
+{
+  [JsonPropertyName("id")]
+  [JsonProperty("id")]
+  public string Id { get; set; }
+}
+
+public class MyValue
+{
+  [JsonPropertyName("prop1")]
+  [JsonProperty("prop1")]
+  public string Prop1 { get; set; }
+
+  [JsonPropertyName("prop2")]
+  [JsonProperty("prop2")]
+  public string Prop2 { get; set; }
+
+  [JsonPropertyName("prop3")]
+  [JsonProperty("prop3")]
+  public bool Prop3 { get; set; }
+
+  [JsonPropertyName("prop4")]
+  [JsonProperty("prop4")]
+  public double Prop4 { get; set; }
+
+  [JsonPropertyName("prop5")]
+  [JsonProperty("prop5")]
+  public int Prop5 { get; set; }
+}

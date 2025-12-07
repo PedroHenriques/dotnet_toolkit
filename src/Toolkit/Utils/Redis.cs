38ac313d49d6577c87d026b6c76a2935a837459b
Pedro Henriques <pedro@pedrojhenriques.com>
2025-12-07 19:20:39 +0000

    Update Redis PrepareInputs to handle new arguments.

--- src/Toolkit/Utils/Redis.cs
+++ src/Toolkit/Utils/Redis.cs
@@ -7,12 +7,18 @@ namespace Toolkit.Utils;
 [ExcludeFromCodeCoverage(Justification = "Not unit testable due to the instantiation of classes from the Redis SDK is done.")]
 public class Redis
 {
-  public static RedisInputs PrepareInputs(ConfigurationOptions conOpts, string consumerGroupName)
+  public static RedisInputs PrepareInputs(
+    ConfigurationOptions conOpts, string consumerGroupName, ILogger? logger = null,
+    string? activitySourceName = null, string? activityName = null
+  )
   {
     return new RedisInputs
     {
       Client = ConnectionMultiplexer.Connect(conOpts),
       ConsumerGroupName = consumerGroupName,
+      Logger = logger,
+      ActivitySourceName = activitySourceName,
+      ActivityName = activityName,
     };
   }
 }
